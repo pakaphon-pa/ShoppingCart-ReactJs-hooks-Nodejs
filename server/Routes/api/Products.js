69f3cbb3684a4749a15af00752b7288ee8e6b1da
MAIMEEARAI <pakaphon23@gmail.com>
2019-10-16 10:01:45 +0700

    second commit

--- server/Routes/api/Products.js
+++ server/Routes/api/Products.js
@@ -3,9 +3,9 @@ const router = express.Router()
 
 
 const database = [
-    { name : "red tshirt " , img: "https://4.imimg.com/data4/MS/GX/MY-9185795/mens-polo-red-t-shirt-500x500.jpg" , price:150 , id : 1},
-    { name : "yellow tshirt " , img: "http://cdn.shopify.com/s/files/1/0232/3963/products/plain-yellow-tshirt_grande.jpg?v=1449214453" , price:180 , id : 2},
-    { name : "green tshirt ", img:"https://4.imimg.com/data4/AE/MC/MY-1044551/plain-t-shirt-500x500.jpg" , price:200 , id : 3}
+    { name : "HTML COURSE " , img: "https://5.imimg.com/data5/TI/NN/GLADMIN-54744791/html-and-html5-500x500.png" , price:150 , id : 1},
+    { name : "CSS COURSE " , img: "https://i0.wp.com/sidneyandfriends.com/wp-content/uploads/2016/03/css-logo.png?fit=500%2C500&ssl=1" , price:180 , id : 2},
+    { name : "NODEJS COURSE ", img:"https://webdesignledger.com/wp-content/uploads/2015/09/00-featured-nodejs.jpg" , price:200 , id : 3}
 ]
 
 router.get('/' , (req , res , next ) =>{
